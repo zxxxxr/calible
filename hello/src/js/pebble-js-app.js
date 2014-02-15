dab51437c7d2abff675d8bc25959e0cddced0c50
xiuruo zhang <zxxxxr@gmail.com>
2014-02-15 04:48:00 -0500

    distance calculation

--- hello/src/js/pebble-js-app.js
+++ hello/src/js/pebble-js-app.js
@@ -1,3 +1,6 @@
+var points = [];
+var startTime = undefined;
+
 Pebble.addEventListener("ready",
 function(e) {
   console.log("connect!" + e.ready);
@@ -27,5 +30,45 @@ function(e) {
   if (e.payload.z) {
     console.log("z = " + z);
   }
+
 });
 
+
+var getDistance = function (sorted) {
+  var vx = 0;
+  var vy = 0;
+  var vz = 0;
+  var dis = 0;
+
+  var ax0 = sorted[0].x;
+  var ay0 = sorted[0].y;
+  var az0 = sorted[0].z;
+
+  for (var i = 1; i < sorted.length; i++) {
+    var dt = sorted[i].time - sorted[i-1].time;
+   
+    var ax = sorted[i].x - ax0;
+    var ay = sorted[i].y - ay0;
+    var az = sorted[i].z - az0;
+
+    var dx = vx + 0.5 * ax * dt * dt;
+    var dy = vy + 0.5 * ay * dt * dt;
+    var dz = vz + 0.5 * az * dt * dt;
+
+    vx += ax * dt;
+    vy += ay * dt;
+    vz += az * dt;
+
+    var s  = Math.sqrt(dx*dx + dy*dy + dz*dz);
+    console.log('s = ' + s);
+    dis += s;
+  }
+  console.log(dis);
+};
+
+/*
+var point1 = {time: 1 , x:0 , y:2 , z:0};
+var point2 = {time: 2 , x:1 , y:0 , z:0};
+var point3 = {time: 3 , x:0 , y:0 , z:1};
+getDistance([point1, point2, point3]);
+*/
