7fe6fbb2d27b7928d8e373d711321fc9092dcaec
xiuruo zhang <zxxxxr@gmail.com>
2014-02-15 05:51:21 -0500

    change js distance

--- hello/src/js/pebble-js-app.js
+++ hello/src/js/pebble-js-app.js
@@ -11,9 +11,10 @@ function(e) {
 Pebble.addEventListener("appmessage",
 function(e) {
   console.log("message");
-  console.log(e);
   if (!e.payload.hasNext) {
     console.log("Data ends.");
+    console.log("  ");
+    getDistance(points);
     return;
   }
   if (e.payload.time_s) {
@@ -32,6 +33,7 @@ function(e) {
     console.log("z = " + e.payload.z);
   }
 
+  points.push(e.payload);
 });
 
 
@@ -45,12 +47,16 @@ var getDistance = function (sorted) {
   var ay0 = sorted[0].y;
   var az0 = sorted[0].z;
 
+  sorted[0].time = sorted[0].time_s + sorted[0].time_ms / 1000;
+  //startTime = sorted[0].time;
+
   for (var i = 1; i < sorted.length; i++) {
+    sorted[i].time = (sorted[i].time_s) + (sorted[i].time_ms/1000);
     var dt = sorted[i].time - sorted[i-1].time;
    
-    var ax = sorted[i].x - ax0;
-    var ay = sorted[i].y - ay0;
-    var az = sorted[i].z - az0;
+    var ax = (sorted[i].x - ax0) / 100;
+    var ay = (sorted[i].y - ay0) / 100;
+    var az = (sorted[i].z - az0) / 100;
 
     var dx = vx + 0.5 * ax * dt * dt;
     var dy = vy + 0.5 * ay * dt * dt;
@@ -61,10 +67,10 @@ var getDistance = function (sorted) {
     vz += az * dt;
 
     var s  = Math.sqrt(dx*dx + dy*dy + dz*dz);
-    console.log('s = ' + s);
+    //console.log('s = ' + s);
     dis += s;
   }
-  console.log(dis);
+  console.log("DISTANCE = " + dis);
 };
 
 /*
