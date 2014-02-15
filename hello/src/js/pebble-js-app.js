3c7fbc67ae98fa786dd20ab40f5b58fb86996e04
xiuruo zhang <zxxxxr@gmail.com>
2014-02-15 02:41:36 -0500

    Add js files and config

---
+++ hello/src/js/pebble-js-app.js
@@ -0,0 +1,33 @@
+Pebble.addEventListener("ready",
+                        function(e) {
+                          console.log("connect!" + e.ready);
+                          console.log(e.type);
+                        });
+
+// Set callback for appmessage events
+Pebble.addEventListener("appmessage",
+                        function(e) {
+                          console.log("message");
+                          if (!e.playload.hasNext) {
+                            console.log("Data ends");
+                            return;
+                          }
+
+                          if (e.payload.time_s) {
+                            console.log("time_s = " + e.payload.s);
+                          }
+                          if (e.payload.time_ms) {
+                            console.log("time_ms = " + e.payload.ms);
+                          }
+                          if (e.payload.x) {
+                            console.log("x = " + x);
+                          }
+                          if (e.payload.y) {
+                            console.log("y = " + y);
+                          }
+                          if (e.payload.z) {
+                            console.log("z = " + z);
+                          }
+
+                        });
+
