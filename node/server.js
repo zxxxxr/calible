c4d4084eb113ba7b526f566208dd29364f059941
Jim Chen <knh.jabbany@gmail.com>
2014-02-15 21:16:42 -0500

    Added server

--- node/server.js
+++ node/server.js
@@ -1,7 +1,12 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
+	if(req.path == "/json"){
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		res.end();
+		return;
+	}
+	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.end('Web Server Inited\n');
-}).listen(1337);
+}).listen(8080);
 
