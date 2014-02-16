0591d024e44609267e5bb43a048d3396e1c675f9
Jim Chen <knh.jabbany@gmail.com>
2014-02-15 21:26:46 -0500

    Working server

--- node/server.js
+++ node/server.js
@@ -1,12 +1,19 @@
-var http = require('http');
+var http = require('http'), fs = require('fs');
 
 http.createServer(function (req, res) {
-	if(req.path == "/json"){
-		res.writeHead(200, {'Content-Type': 'application/json'});
-		res.end();
+	if(req.url == "/json"){
+		fs.readFile('../data/good.json', function(err,data){ 
+			if (err) throw err;
+			res.writeHead(200, {'Content-Type': 'application/json'});
+			res.end(data);
+		});
+		
 		return;
 	}
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('Web Server Inited\n');
+	fs.readFile('render.html', function(err,data){ 
+		if (err) throw err;
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end(data);
+	});
 }).listen(8080);
 
